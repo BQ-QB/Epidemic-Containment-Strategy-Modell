5b8e005e4221aad3c28444dcba2ef19ef7188425
MaxJisonsund <95430788+MaxJisonsund@users.noreply.github.com>
2022-02-08 12:39:51 +0100

    Jag har lagt till funtionen update_position() och programmet fungerar fortfarande

--- sir_based.py
+++ sir_based.py
@@ -13,10 +13,7 @@ canvas.place(x=res/20, y=res/20, height=res, width=res)
 ccolor = ['#0008FF', '#DB0000', '#12F200', '#68228B', '#000000']
 
 
-
-
 def plot_sir():
-
     temp1 = SH.shape[0]
     temp1 = np.array([i for i in range(temp1)])
     fig = plt.figure()
@@ -33,11 +30,19 @@ def plot_sir():
     ax.legend()
     plt.show()
 
+def update_position():
+    steps_x_or_y = np.random.rand(n)
+    steps_x = steps_x_or_y < D / 2
+    steps_y = (steps_x_or_y > D / 2) & (steps_x_or_y < D)
+    nx = (x + np.sign(np.random.randn(n)) * steps_x) % l
+    ny = (y + np.sign(np.random.randn(n)) * steps_y) % l
+    for i in np.where(((isolated != 0) | (S == 3))):
+        nx[i] = x[i]
+        ny[i] = y[i]
+    return nx, ny
 
 show_plot = Button(tk, text='Plot', command=plot_sir)
 show_plot.place(relx=0.05, rely=0.85, relheight=0.06, relwidth=0.15)
-
-
 
 # Parameters of the simulation
 n = 1000     # Number of agents 
@@ -60,7 +65,7 @@ x = np.floor(np.random.rand(n)*l)          # x coordinates
 y = np.floor(np.random.rand(n)*l)          # y coordinates  
 S = np.zeros(n)                            # status array, 0: Susceptiple, 1: Infected, 2: recovered, 3: Dead 
 isolated = np.zeros(n)                     # Isolation array, 0: not isolated, 1: Is currently in isolation                   # test array; 0: Should not be isolated, 1: Positive test, should be isolated 
-temperatures  = np.zeros(n)                            # temperature array
+temperatures = np.zeros(n)                            # temperature array
 S[1:initial_infected] = 1              # Infect agents that are close to center 
 
 nx = x                           # updated x                  
@@ -80,12 +85,13 @@ def set_temps():
     for i in np.where(S == 1)[0]:
         temperatures [i] = np.random.normal(40,1)
 
-    for i in np.where(temperatures  == 0)[0]:
+    for i in np.where(temperatures == 0)[0]:
         temperatures [i] = np.random.normal(36,1)
         
 #test
 
 # Modifiable parameters by the user 
+
 
 D_noll = 0.8
 D_reduced = 0.1
@@ -98,21 +104,11 @@ My = 0.00
 start_lock = 50
 lockdown_enabled = True
 test_capacity = 100
-
 set_temps()
 t = 0
 
 while t < 1000 and list(np.where(S == 1)[0]):
-
-    steps_x_or_y = np.random.rand(n)
-    steps_x = steps_x_or_y < D/2
-    steps_y = (steps_x_or_y > D/2) & (steps_x_or_y < D)
-    nx = (x + np.sign(np.random.randn(n)) * steps_x) % l 
-    ny = (y + np.sign(np.random.randn(n)) * steps_y) % l
-    for i in np.where(((isolated != 0) | (S == 3))):
-        nx[i] = x[i]
-        ny[i] = y[i]
-
+    nx, ny = update_position()
     for i in np.where((isolated != 1) & (S == 1) & (np.random.random(n) < B))[0]:     # loop over infecting agents
         temperatures [(x == x[i]) & (y == y[i]) & (S == 0)] = np.random.normal(40, 1)          # Raise newly sick agents temperatures
         S[(x == x[i]) & (y == y[i]) & (S == 0)] = 1         # Susceptiples together with infecting agent becomes infected
@@ -144,7 +140,7 @@ while t < 1000 and list(np.where(S == 1)[0]):
     
     # Tests sick agents, if positive test then set in isolation and isolate neighbours in contactmatrix
     if t > 5:
-        
+
         test_priority = np.argsort(temperatures)
         i = 0
         tests_made = 0
@@ -166,7 +162,8 @@ while t < 1000 and list(np.where(S == 1)[0]):
     # lockdown_enabled loop
     if start_lock < t < start_lock + 200 and lockdown_enabled:
         D = D_reduced
-    else: D = D_noll
+    else:
+        D = D_noll
 
     x = nx                                              # Update x 
     y = ny                                              # Update y 
