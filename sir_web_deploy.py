3988806554786940d487399ad8b50a679e2aa9ed
BQ <jesper.bergquist1@gmail.com>
2022-03-03 14:03:33 +0100

    TEST WEB APP

--- sir_web_deploy.py
+++ sir_web_deploy.py
@@ -7,7 +7,7 @@ app = flask.Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return "YEP COCK"
+    return "YEP COCK CALLE ÄR EN RIKTIG G"
 
 if __name__ == "__main__":
     app.secret_key = 'Epidemic_Enjoyers'
